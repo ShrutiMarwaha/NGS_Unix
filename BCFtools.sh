b98ac7cb899c971c1ec0bc074dc0ce33d83256a0
Shruti Marwaha <shruti.marwaha@gmail.com>
2016-09-11 18:20:31 -0700

    tools for data wrangling for NGS data

---
+++ BCFtools.sh
@@ -0,0 +1,5 @@
+cd ~/Documents/Softwares/
+cd bcftools-1.2
+make
+make /usr/bin install
+which bcftools
---
+++ Samtools.sh
@@ -0,0 +1,21 @@
+########## Samtools installation ####################
+# Download from http://www.htslib.org/download/
+curl -O https://github.com/samtools/samtools/releases/download/1.2/samtools-1.2.tar.bz2
+cd ~/Documents/Softwares/
+cd samtools-1.2
+make
+sudo make install
+# add location where you installed samtools or move (mv) the folder to /usr/local/bin
+vi ~/.bash_profile
+# PATH="/Users/shruti/Documents/Softwares/samtools-1.2/bin:${PATH}"
+source ~/.bash_profile
+# check if its installed properly and location
+which samtools
+# help
+samtools
+######################################################
+
+# move to the directory where bam file is stored
+cd ~/Documents/Softwares/ngs/variants/
+# flagstat command provides simple statistics about the data in bam file
+samtools flagstat na12878_sorted.bam 
